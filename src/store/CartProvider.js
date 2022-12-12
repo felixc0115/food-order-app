96837720cdb1a59a801a59e4695cd40013b6de48
Felix Chen <fechen1993@gmail.com>
2022-12-12 12:43:34 -0500

    Work on cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,6 +1,20 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  return defaultCartState;
+};
 
 const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
   const addItemToCartHandler = (item) => {};
 
   const removeItemCartHandler = (item) => {};
