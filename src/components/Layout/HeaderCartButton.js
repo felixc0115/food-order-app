a4e9c842ef2592abef4d2e719ba7c6e2f96bbfba
Felix Chen <fechen1993@gmail.com>
2022-12-17 16:08:25 -0500

    Add bump animation to header cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,36 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numberOfItemsInCart = cartCtx.items.reduce(
+  const { items } = cartCtx;
+  const numberOfItemsInCart = items.reduce(
     (total, item) => total + item.amount,
     0
   );
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button onClick={props.onOpen} className={classes.button}>
+    <button className={btnClasses} onClick={props.onOpen}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
