549470e7315ca6dafb3b645e05fd991b57ba7b41
Felix Chen <fechen1993@gmail.com>
2023-01-24 09:53:19 -0500

    Work on order form validation logic

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,27 +1,67 @@
 import classes from "./Checkout.module.css";
+import { useRef } from "react";
+
+const isEmpty = (value) => {
+  return value.trim() === "";
+};
+
+const isNotFiveDigits = (value) => {
+  return value.trim().length !== 5;
+};
 
 const Checkout = (props) => {
+  const nameInputRef = useRef("");
+  const streetInputRef = useRef("");
+  const postalCodeInputRef = useRef("");
+  const cityInputRef = useRef("");
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const customerInfo = {
+      name: nameInputRef.current.value,
+      street: streetInputRef.current.value,
+      postalCode: postalCodeInputRef.current.value,
+      city: cityInputRef.current.value,
+    };
+
+    const enteredNameIsValid = !isEmpty(customerInfo.name);
+    const enteredStreetIsValid = !isEmpty(customerInfo.street);
+    const enteredPostalCodeIsValid = !isNotFiveDigits(customerInfo.postalCode);
+    const enteredCityIsValid = !isEmpty(customerInfo.city);
+
+    if (
+      !enteredNameIsValid ||
+      !enteredStreetIsValid ||
+      !enteredPostalCodeIsValid ||
+      !enteredCityIsValid
+    ) {
+      return;
+    }
+    console.log(customerInfo);
+    nameInputRef.current.value = "";
+    streetInputRef.current.value = "";
+    postalCodeInputRef.current.value = "";
+    cityInputRef.current.value = "";
   };
 
   return (
     <form className={classes.form} onSubmit={confirmHandler}>
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" />
+        <input ref={nameInputRef} type="text" id="name" />
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
-        <input type="text" id="street" />
+        <input ref={streetInputRef} type="text" id="street" />
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
-        <input type="text" id="postal" />
+        <input ref={postalCodeInputRef} type="text" id="postal" />
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
-        <input type="text" id="city" />
+        <input ref={cityInputRef} type="text" id="city" />
       </div>
       <div className={classes.actions}>
         <button className={classes.submit}>Confirm</button>
