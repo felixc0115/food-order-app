56cf19a9270dfd0001cec058f28906b85fae0e5c
Felix Chen <fechen1993@gmail.com>
2023-01-24 11:46:29 -0500

    Finish order form validation logic

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,5 +1,5 @@
 import classes from "./Checkout.module.css";
-import { useRef } from "react";
+import { useRef, useReducer } from "react";
 
 const isEmpty = (value) => {
   return value.trim() === "";
@@ -9,7 +9,53 @@ const isNotFiveDigits = (value) => {
   return value.trim().length !== 5;
 };
 
+const validationReducer = (state, action) => {
+  if (action.type === "INVALID_NAME") {
+    return {
+      ...state,
+      nameIsValid: false,
+    };
+  }
+
+  if (action.type === "INVALID_STREET") {
+    return {
+      ...state,
+      streetIsValid: false,
+    };
+  }
+  if (action.type === "INVALID_POSTAL") {
+    return {
+      ...state,
+      postalCodeIsValid: false,
+    };
+  }
+  if (action.type === "INVALID_CITY") {
+    return {
+      ...state,
+      cityIsValid: false,
+    };
+  }
+
+  if (action.type === "RESET_VALIDITY") {
+    return initialInputValidity;
+  }
+
+  return initialInputValidity;
+};
+
+const initialInputValidity = {
+  nameIsValid: true,
+  streetIsValid: true,
+  postalCodeIsValid: true,
+  cityIsValid: true,
+};
+
 const Checkout = (props) => {
+  const [inputValidityState, dispatch] = useReducer(
+    validationReducer,
+    initialInputValidity
+  );
+
   const nameInputRef = useRef("");
   const streetInputRef = useRef("");
   const postalCodeInputRef = useRef("");
@@ -17,6 +63,8 @@ const Checkout = (props) => {
 
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    dispatch({ type: "RESET_VALIDITY" });
 
     const customerInfo = {
       name: nameInputRef.current.value,
@@ -30,14 +78,28 @@ const Checkout = (props) => {
     const enteredPostalCodeIsValid = !isNotFiveDigits(customerInfo.postalCode);
     const enteredCityIsValid = !isEmpty(customerInfo.city);
 
-    if (
-      !enteredNameIsValid ||
-      !enteredStreetIsValid ||
-      !enteredPostalCodeIsValid ||
-      !enteredCityIsValid
-    ) {
+    const formIsValid =
+      enteredNameIsValid &&
+      enteredStreetIsValid &&
+      enteredPostalCodeIsValid &&
+      enteredCityIsValid;
+
+    if (!formIsValid) {
+      if (!enteredNameIsValid) {
+        dispatch({ type: "INVALID_NAME" });
+      }
+      if (!enteredStreetIsValid) {
+        dispatch({ type: "INVALID_STREET" });
+      }
+      if (!enteredPostalCodeIsValid) {
+        dispatch({ type: "INVALID_POSTAL" });
+      }
+      if (!enteredCityIsValid) {
+        dispatch({ type: "INVALID_CITY" });
+      }
       return;
     }
+
     console.log(customerInfo);
     nameInputRef.current.value = "";
     streetInputRef.current.value = "";
@@ -50,18 +112,26 @@ const Checkout = (props) => {
       <div className={classes.control}>
         <label htmlFor="name">Your Name</label>
         <input ref={nameInputRef} type="text" id="name" />
+        {!inputValidityState.nameIsValid && <p>Please enter a valid name</p>}
       </div>
       <div className={classes.control}>
         <label htmlFor="street">Street</label>
         <input ref={streetInputRef} type="text" id="street" />
+        {!inputValidityState.streetIsValid && (
+          <p>Please enter a valid street</p>
+        )}
       </div>
       <div className={classes.control}>
         <label htmlFor="postal">Postal Code</label>
         <input ref={postalCodeInputRef} type="text" id="postal" />
+        {!inputValidityState.postalCodeIsValid && (
+          <p>Please enter a valid postal code</p>
+        )}
       </div>
       <div className={classes.control}>
         <label htmlFor="city">City</label>
         <input ref={cityInputRef} type="text" id="city" />
+        {!inputValidityState.cityIsValid && <p>Please enter a valid city</p>}
       </div>
       <div className={classes.actions}>
         <button className={classes.submit}>Confirm</button>
